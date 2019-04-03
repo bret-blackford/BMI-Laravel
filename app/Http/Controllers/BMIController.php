ab4a42c039e18c0860cc9139c17a5addf9c637fd
Bret Blackford <bret_blackford@yahoo.com>
2019-04-02 19:49:28 -0500

    made /valuation route a POST

--- app/Http/Controllers/BMIController.php
+++ app/Http/Controllers/BMIController.php
@@ -5,52 +5,52 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use IanLChapman\PigLatinTranslator\Parser;
 
-class BMIController extends Controller
-{
+class BMIController extends Controller {
+
     //
     public function index() {
-	dump( 'in app/Http/Controllers/BMIController - just prep' );
-        return view('bmi.bmi')->with( [
-            'name' => 'Bret',
-            'dob' => null,
-            'gender' => null,
-            'heightFeet' => 6,
-            'heightInches' => 1,
-            'weight' => 177,
-                ]);
+        dump('in app/Http/Controllers/BMIController - just prep');
+        return view('bmi.bmi')->with([
+                    'name' => 'Bret',
+                    'dob' => null,
+                    'gender' => null,
+                    'heightFeet' => 6,
+                    'heightInches' => 1,
+                    'weight' => 177,
+        ]);
     }
 
     public function calc(Request $request) {
-        $name = $request->session()->get('name','');
+        $name = $request->session()->get('name', '');
         $dob = $request->session()->get('dob', null);
-        $gender = $request->session()->get('gender',null);
-        $heightFeet = $request->session()->get('heightFeet',null);
-        $heightInches = $request->session()->get('heightInches',null);
-        $weight = $request->session()->get('weight',null);
-        
-	//return 'in calc() : perform calculations here';
+        $gender = $request->session()->get('gender', null);
+        $heightFeet = $request->session()->get('heightFeet', null);
+        $heightInches = $request->session()->get('heightInches', null);
+        $weight = $request->session()->get('weight', null);
+
+        //return 'in calc() : perform calculations here';
         //return view('bmi.bmi')->with( ['title' => $title] );
-        return view('bmi.show')->with( [
-            'name' => $name,
-            'dob' => $dob,
-            'gender' => $gender,
-            'heightFeet' => $heightFeet,
-            'heightInches' => $heightInches,
-            'weight' => $weight,
-                ]);
+        return view('bmi.show')->with([
+                    'name' => $name,
+                    'dob' => $dob,
+                    'gender' => $gender,
+                    'heightFeet' => $heightFeet,
+                    'heightInches' => $heightInches,
+                    'weight' => $weight,
+        ]);
     }
 
     public function practiceX() {
-	$translator = new Parser();
-	$translation = $translator->translate('Hello world !');
-	dump( $translation );
-	dump( 'in practiceX() of the BMIController' );
+        $translator = new Parser();
+        $translation = $translator->translate('Hello world !');
+        dump($translation);
+        dump('in practiceX() of the BMIController');
     }
-    
+
     public function validate(Request $request) {
-        
+
         dump($request);
-        
+
         $request->validate([
             'name' => 'required',
             'dob' => 'required',
@@ -61,5 +61,5 @@ class BMIController extends Controller
         ]);
         dump('in BMIController validate()');
     }
-    
+
 }
