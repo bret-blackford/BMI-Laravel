625f20f6716d8c91030616217a05ecea025100c5
Bret Blackford <bret_blackford@yahoo.com>
2019-04-04 18:32:56 -0500

    added use DateTime

--- app/Http/Controllers/BMIController.php
+++ app/Http/Controllers/BMIController.php
@@ -98,6 +98,7 @@ class BMIController extends Controller {
         $response .= " and have a body mass index (BMI) of : <b>" . $bmi . "</b>%";
         $response .= " which classifies you as <b>" . $status . "</b>";
 
+        dump( 'response' . $response );
         return redirect('/bmi')->with([
             'response' => $response
         ]);
